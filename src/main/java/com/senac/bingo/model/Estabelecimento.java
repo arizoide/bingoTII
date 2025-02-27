88958cf7253e133da70e881649bf458424514a97
ARI THOMAZINI <ari.thomazini@senacsp.edu.br>
2025-02-26 22:05:02 -0300

    Criacao de entities

---
+++ src/main/java/com/senac/bingo/model/Estabelecimento.java
@@ -0,0 +1,56 @@
+package com.senac.bingo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Estabelecimento {
+    @Id
+    private Long cnpj;
+    private String razaoSocial;
+    private String email;
+    private String telefone;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(Long cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
